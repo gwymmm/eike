d26839010ae828b4b336f0121b49cee6dd24ee9b
gwymmm <dn8733@yahoo.com>
2019-11-01 21:28:08 +0100

    First GUI layout ready

--- source/geike.adb
+++ source/geike.adb
@@ -25,9 +25,33 @@ with gtk.file_chooser_button;
 with gtk.file_chooser;
 with gtk.button;
 
+with gtk.frame;
+with gtk.text_tag_table;
+with gtk.text_tag;
+with gtk.text_buffer;
+with gtk.text_view;
+
+with pango.font;
+with pango.enums; 
+with gtk.enums;
+with gtk.text_iter;
+
+with gtk.scrolled_window;
+
 procedure geike is
+lab_frame : gtk.frame.gtk_frame;
 my_label : gtk.label.gtk_label;
+--my_label2 : gtk.label.gtk_label;
 --my_vbox : gtk.box.gtk_box;
+tt : gtk.text_tag_table.gtk_text_tag_table;
+tag : gtk.text_tag.gtk_text_tag;
+tag2 : gtk.text_tag.gtk_text_tag;
+text_buf : gtk.text_buffer.gtk_text_buffer;
+text_vw : gtk.text_view.gtk_text_view;
+text_frame : gtk.frame.gtk_frame;
+it : gtk.text_iter.gtk_text_iter;
+it2 : gtk.text_iter.gtk_text_iter;
+scr : gtk.scrolled_window.gtk_scrolled_window;
 begin
 gtk.main.init;
 eike_gui.main_window := gtk.window.gtk_window_new;
@@ -82,14 +106,87 @@ my_label := gtk.label.gtk_label_new;
 --gtk.label.set_markup(my_label, ("<span size=""x-large"" weight=""ultrabold"""
 --          & "background=""darkblue"" color=""white"">"
 --          & "This label has <i>markup</i>!</span>"));
-gtk.label.set_markup(my_label, ("<span size=""x-large"" weight=""ultrabold"""
-          & " color=""green"">"
-          & "RECHNUNG OK (Syntax: UBL Invoice)</span> <span>" & character'val(16#A#) & "und etwas mehr Text... </span>"));
+gtk.label.set_markup(my_label, --("<span size=""x-large"" weight=""ultrabold"""
+          --& " background=""green"" color=""white"">"
+          --& 
+          ("<span font_desc=""Courier bold"" size=""large"" color=""green"">UBL Invoice Syntax, XRechnung konform </span>"));
 gtk.label.set_selectable(my_label, true);
+
+lab_frame := gtk.frame.gtk_frame_new("Statusreport");
+gtk.frame.add(lab_frame, my_label);
+--my_label2 := gtk.label.gtk_label_new("Anderer Inhalt");
+--gtk.frame.add(lab_frame, my_label2);
+
+
+
+-------------------------
+gtk.text_tag_table.gtk_new(tt);
+gtk.text_tag.gtk_new (tag, "courier_tag");
+gtk.text_tag_table.add(tt, tag);
+pango.font.set_property(tag, gtk.text_tag.font_desc_property, pango.font.from_string("courier 14"));
+pango.enums.set_property(tag, gtk.text_tag.weight_property, pango.enums.pango_weight_ultraheavy);
+gtk.text_tag.gtk_new (tag2, "weight_tag");
+gtk.text_tag_table.add(tt, tag2);
+pango.enums.set_property(tag2, gtk.text_tag.weight_property, pango.enums.pango_weight_ultraheavy);
+
+text_buf := gtk.text_buffer.gtk_text_buffer_new(tt);
+text_vw := gtk.text_view.gtk_text_view_new_with_buffer(text_buf);
+gtk.text_buffer.get_end_iter(text_buf, it);
+--gtk.text_buffer.insert_at_cursor(text_buf, 
+--"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY");
+gtk.text_buffer.insert_with_tags(text_buf, it,
+"In diesem Textfeld sollen die Rechnungsdaten angezeigt werden." & character'val(16#A#) &
+"Der Text kann markiert und in die Zwischenablage kopiert werden." & character'val(16#A#) &
+"Ein Verändern des Textes ist aber nicht möglich." & character'val(16#A#) &
+"Für längere Texte kann die Scrollbar auf der rechten Seite genutzt werden, " &
+"um im Textfenster verschiedene Bereiche auszuwählen." & character'val(16#A#) &
+--2#11101100# 2#10010101# 2#10011001#
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY" &
+"Name: XY" & character'val(16#A#) & "Adresse: XXX YYYY",
+tag);
+
+gtk.text_buffer.get_start_iter(text_buf, it);
+gtk.text_buffer.get_end_iter(text_buf, it2);
+
+--gtk.text_buffer.apply_tag(text_buf, tag2, it, it2);
+
+gtk.text_view.set_editable(text_vw, false);
+-------------------------
+
+scr := gtk.scrolled_window.gtk_scrolled_window_new;
+text_frame := gtk.frame.gtk_frame_new("Inhalt");
+
+gtk.frame.add(text_frame, scr);
+gtk.scrolled_window.add(scr, text_vw);
 
 gtk.box.pack_start(eike_gui.top_level_vbox, eike_gui.first_hbox, expand => false, fill => false, padding => 10);
 gtk.box.pack_start(eike_gui.top_level_vbox, eike_gui.second_hbox, expand => false, fill => false, padding => 10);
-gtk.box.pack_start(eike_gui.top_level_vbox, my_label, expand => false, fill => false, padding => 10);
+gtk.box.pack_start(eike_gui.top_level_vbox, lab_frame, expand => false, fill => false, padding => 10);
+gtk.box.pack_start(eike_gui.top_level_vbox, text_frame, expand => true, fill => true, padding => 10);
 
 gtk.window.add(eike_gui.main_window, eike_gui.top_level_vbox);
 
